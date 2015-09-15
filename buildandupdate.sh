5c3553f471c9e18c919f67d9c3178c3943f40936
Kyle Galloway <kylegalloway@tclmacw1623.local>
2015-09-15 16:59:23 -0500

    new post

--- buildandupdate.sh
+++ buildandupdate.sh
@@ -10,11 +10,11 @@ printf "\nBuilding and pushing to GitHub.\n\n"
 {
     jekyll build && \
     cd _site && \
-    git add -A && \
+    git add . && \
     git commit -am "$1" && \
     git push origin master && \
     cd .. && \
-    git add -A && \
+    git add . && \
     git commit -am "$1" && \
     git push origin source
 } &> /dev/null
